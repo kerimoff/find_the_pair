179eddc4010db31b807fe7dcd524f67d7397b1d4
Nurlan Kerimov <34273025+kerimoff@users.noreply.github.com>
2020-09-17 09:35:59 +0300

    changed the script js to make more OO

--- script.js
+++ script.js
@@ -1,117 +1,137 @@
-//HTML ELEMENTS
-let cardDivs = document.querySelectorAll('.card');
-// let arrCardDivs = [].slice.call(cardDivs)
-
-//CREATE ARRAY OF IMGAGE PATHS
-let imgArr = new Array();
-
 Array.prototype.shuffle = function(){
-  for(let i = imgArr.length-1; i > 0; i--){
+  // change input var name
+  for(let i = this.length-1; i > 0; i--){
     const j = Math.floor(Math.random() * i)
-    const temp = imgArr[i]
-    imgArr[i] = imgArr[j]
-    imgArr[j] = temp
+    // can use pattern matching
+    const temp = this[i]
+    this[i] = this[j]
+    this[j] = temp
   }
 }
-
-for (let i=0; i< 8;i++){
-  imgArr.push("assets/visuals/p"+(i+1)+".jpg");
-  imgArr.push("assets/visuals/p"+(i+1)+".jpg");
-}
-imgArr.shuffle();
-
-
-
-//CREATING CARD CLASS AND ADDING INSTANCES TO AN ARRAY
-
-let cardsArray = new Array();
 class Card{
-  constructor (card, position, isOpen, imgSource, isFound){
-    this.card = card;
+  constructor (position, isOpen, imgSource, isFound){
     this.position = position;
     this.isOpen = isOpen;
     this.imgSource = imgSource;
     this.isFound = isFound;
   }
+
+  open() {
+    this.isOpen = true
+    document.getElementById(this.position).classList.add('display')
+  }
+
+  close() {
+    this.isOpen = false
+    document.getElementById(this.position).classList.remove('display')
+  }
+
+  setFound(){
+    this.isFound = true;
+    document.getElementById(this.position).classList.add('display')
+    this.isOpen = false;
+  }
 }
 
-for (let i = 0; i < cardDivs.length; i++) {
-  cardsArray.push(new Card(cardDivs[i], i, false, imgArr[i], false))
+class Game{
+  constructor (cardsCollection, username, score){
+    this.cardsCollection = cardsCollection;
+    this.username = username;
+    this.score = score;
+    this.size = cardsCollection.length;
+  }
+
+  // Parameters
+  get isOver() {
+    let foundCards = this.cardsCollection.filter(card => card.isFound);
+    return foundCards.length == this.size;
+  }
+  
+  get hasOpenCard() {
+    return this.openCards.length > 0;
+  }
+
+  get openCards() {
+    return this.cardsCollection.filter(card => card.isOpen);
+  }
+  
+  get openCard() {
+    if (this.hasOpenCard) {
+      return this.openCards[0]
+    } else {
+      return false;
+    }
+  }
+  
+  closeOpenCards() {
+    this.openCards.forEach(card => {
+      if (!card.isFound) card.close()
+    })
+  }
 }
 
+//HTML ELEMENTS
+let cardDivs = document.querySelectorAll('.card');
 
-//LISTENING FOR CLICK: CHANING BLANK CARD TO IMG
-//variables to keep information about currently open card
-let waitingCardImg = ''
-let waitingCardObject;
-let waitingCardImgSrc;
+let numberOfImages = cardDivs.length;
+
+//CREATE ARRAY OF IMGAGE PATHS
+let imgArr = new Array();
+
+for (let i=0; i<numberOfImages / 2; i++){
+  imgArr.push("assets/visuals/p"+(i+1)+".jpg");
+  imgArr.push("assets/visuals/p"+(i+1)+".jpg");
+}
+imgArr.shuffle();
+
+//CREATING CARD CLASS AND ADDING INSTANCES TO AN ARRAY
+let cardsArray = new Array();
+for (let i = 0; i < numberOfImages; i++) {
+  cardsArray.push(new Card(i, false, imgArr[i], false))
+}
+
+let game  = new Game(cardsArray, "Nurlan", 0)
 
 //looping through array and listening for events
 cardsArray.forEach((objCard,index) => {
   //create img tag and add img link to divs in object.card(which is an html element)
   let imgTag = document.createElement('img')
   imgTag.setAttribute('src', objCard.imgSource)
-  imgTag.setAttribute('id', index)
+  imgTag.setAttribute('id', objCard.position)
   imgTag.classList.add('imgStyle')
-  objCard.card.appendChild(imgTag);
-  
+
+  cardDivs[index].appendChild(imgTag);
+
   //listen for the click
-  objCard.card.addEventListener('click', function(e){
-    let cardOpen = cardsArray.filter(objCard => objCard.isOpen === true);
-    let cardIsFound = cardsArray.filter(objCard => objCard.isFound === true);
-    // if the game just started
-    if(cardOpen.length === 0){
-      objCard.isOpen = true;
-      let imgId = document.getElementById(objCard.position)
-      imgId.classList.toggle('display')
-      //add informatiom of the current card to waiting variables
-      waitingCardImg = imgId;
-      waitingCardObject = objCard;
-      waitingCardImgSrc = objCard.imgSource
+  cardDivs[index].addEventListener('click', function(e){
+    if (game.openCards.length > 1) {
+      game.closeOpenCards();
     }
     
-    //when there is an open card
-    if(cardOpen.length > 0 ){
-      //if second open card is same as the waiting card
-      if (objCard.imgSource.localeCompare( waitingCardImgSrc)=== 0) {
-        let secondImgId = document.getElementById(objCard.position)
-        secondImgId.classList.toggle('display')
-        
-        //change isFound status to true;
-        waitingCardObject.isFound = true;
-        objCard.isFound = true;
-        
-        //remove isOpen key from current objects
-        delete objCard.isOpen;
-        delete waitingCardObject.isOpen;
-
-        resetWaiting();
-        
+    if (!game.hasOpenCard) {
+      objCard.open()
+    } else {
+      if (objCard.position == game.openCard.position) {
+        return
       }
-      
-      //if second open card different from the waiting card
-      if (objCard.imgSource.localeCompare( waitingCardImgSrc) !== 0 && waitingCardImgSrc.length>0) {
-        let secondImgId = document.getElementById(objCard.position)
-        secondImgId.classList.toggle('display');
-        setTimeout(()=>{
-          secondImgId.classList.toggle('display');
-          waitingCardImg.classList.toggle('display');
-        },1000)
-        
-        objCard.isOpen = false;
-        waitingCardObject.isOpen = false; 
+      if (objCard.imgSource === game.openCard.imgSource) {
+        game.openCard.setFound()
+        objCard.setFound()
+        if (game.isOver) {
+          setTimeout(()=>{
+            alert("The game is over!!!")
+          },500)
+        }
+      } else {
+        objCard.open();
+        // setTimeout(()=>{
+        //   if (!objCard.isFound) objCard.close()
+        //   if (game.hasOpenCard && !game.openCard.isFound) game.openCard.close()
+        // },1000).bind([objCard, game.openCard])
       }
-      cardOpen.pop()
     }
-  })
-});
+    console.log(game.openCards);
+  }); // end eventListener
 
 
-//FUNCTIONS
-function resetWaiting(){
-  waitingCardImg = '';
-  waitingCardObject = {};
-  waitingCardImgSrc = '';
-}
-
-
+}); // end forEach
